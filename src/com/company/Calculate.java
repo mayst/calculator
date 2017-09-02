9eff746134eecd003d0cbf1a7a1fb5d227f9934b
mayst <mayst7809@online.ua>
2017-09-02 19:09:43 +0300

    analyze data and show answer, clear history

--- src/com/company/Calculate.java
+++ src/com/company/Calculate.java
@@ -6,35 +6,87 @@ import java.util.regex.Pattern;
 class Calculate {
     private String history;
     private double[] vars = new double[2];
-    private double operation;
+    private String operation;
+    private double answer;
+    private final static Pattern START_EQUATION_PATTERN = Pattern.compile("^-?[0-9]+.?[0-9]*\\s?(\\+|-|\\*|/)\\s?-?[0-9]+.?[0-9]*$");
+    private final static Pattern EQUATION_PATTERN = Pattern.compile("^\\s?(\\+|-|\\*|/)\\s?-?[0-9]+.?[0-9]*$");
 
     Calculate() {
         System.out.println("Enter equation please:");
         history = "";
     }
 
-    boolean Input_data(String operation) {
-        Pattern p = Pattern.compile("^-?[0-9]+.?[0-9]*\\s?(\\+|-|\\*|/)\\s?-?[0-9]+.?[0-9]*$");
-        Matcher m = p.matcher(operation);
+    boolean inputData(String operation) {
+       if(history.equals("")) {
+           Matcher m = START_EQUATION_PATTERN.matcher(operation);
 
-        if(m.matches()) {
-            history += operation;
-        } else {
-            return false;
-        }
+           if(m.matches()) {
+               history += operation + " ";
+           } else {
+               return false;
+           }
 
-//        -?[0-9]+.?[0-9]*
-        String[] str = operation.split("(\\+|-|\\*|/)");
+           String[] str = operation.split("(\\+|-|\\*|/)");
+           int i = 0;
 
-        for(String s: str) {
-            System.out.println(s);
-        }
+           for(String s: str) {
+               vars[i] = Double.parseDouble(s);
+               operation = operation.replace(s, "");
+               i++;
+           }
+       } else {
+           Matcher m = EQUATION_PATTERN.matcher(operation);
+
+           if(m.matches()) {
+               history += operation + " ";
+           } else {
+               return false;
+           }
+
+           String str = operation.replaceAll("( |\\+|-|\\*|/)", "");
+           vars[0] = answer;
+           vars[1] = Double.parseDouble(str);
+           operation = operation.replace(str, "");
+       }
+
+        this.operation = operation;
 
         return true;
     }
 
-    boolean Commands(String command) {
+    void showAnswer() {
+        switch(this.operation) {
+            case "+":
+                answer = vars[0] + vars[1];
+                break;
+            case "-":
+                answer = vars[0] - vars[1];
+                break;
+            case "*":
+                answer = vars[0] * vars[1];
+                break;
+            case "/":
+                answer = vars[0] / vars[1];
+                break;
+        }
+
+        System.out.println(history);
+        System.out.println(answer);
+    }
+
+    boolean commands(String command) {
         switch(command) {
+            /*case "-back":
+                StringBuffer sb = new StringBuffer(history.trim());
+                sb.delete(sb.lastIndexOf(" "), sb.length());
+                history = sb.toString();
+
+                return true;*/
+            case "-clear":
+                history = "";
+                operation = "";
+
+                return true;
             case "-quite":
                 return false;
             default:
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,16 +6,17 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Scanner sc = new Scanner(System.in);
-        Calculate calc = new Calculate();
-        boolean status = true;
-        String input;
+        try (Scanner sc = new Scanner(System.in)) {
+            Calculate calc = new Calculate();
+            boolean status = true;
+            String input;
 
-        while(status) {
-            if(calc.Input_data(input = sc.nextLine())) {
-                System.out.println("Here will be answer");
-            } else {
-                status = calc.Commands(input);
+            while (status) {
+                if (calc.inputData(input = sc.nextLine())) {
+                    calc.showAnswer();
+                } else {
+                    status = calc.commands(input);
+                }
             }
         }
     }
